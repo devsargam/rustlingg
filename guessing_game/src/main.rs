a3fd5246ea8fbcc784467397a381cfb412331e88
devsargam <sargampoudel100@gmail.com>
2024-02-09 14:57:47 +0530

    finish guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,22 +7,29 @@ fn main() {
 
     println!("Guess the number");
 
-    println!("Please enter your guess: ");
+    loop {
+        println!("Please enter your guess: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Low!!!"),
-        Ordering::Greater => println!("Too Big!!!"),
-        Ordering::Equal => println!("Equal"),
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Low!!!"),
+            Ordering::Greater => println!("Too Big!!!"),
+            Ordering::Equal => {
+                println!("You Win 🎉");
+                break;
+            }
+        }
     }
-
-    println!("You guessed: {guess}");
-    println!("Secret number was: {}", secret_number);
 }
