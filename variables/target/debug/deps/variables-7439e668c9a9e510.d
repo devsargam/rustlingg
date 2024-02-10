89bc3282ec5eb5b80623163f7bac06de069c6fd9
devsargam <sargampoudel100@gmail.com>
2024-02-10 17:47:51 +0530

    feat: add variables

---
+++ variables/target/debug/deps/variables-7439e668c9a9e510.d
@@ -0,0 +1,5 @@
+/Users/sargampoudel/Developer/learning/rust/variables/target/debug/deps/libvariables-7439e668c9a9e510.rmeta: src/main.rs
+
+/Users/sargampoudel/Developer/learning/rust/variables/target/debug/deps/variables-7439e668c9a9e510.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/sargampoudel/Developer/learning/rust/variables/target/debug/variables: /Users/sargampoudel/Developer/learning/rust/variables/src/main.rs
